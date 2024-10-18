4faabf0750c5742c61cce7b9c87574dd1138dd6f
Odilbek798 <odilbekiskandaroviskandarov@gmail.com>
2024-10-18 19:04:10 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './components/card/Card'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+    <div className="wrap">
+      <Card/>
+    </div>
+    </>
+  )
+}
+
+export default App
